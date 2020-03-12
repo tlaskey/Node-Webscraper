b58e8e5b1120b010e9f64762a7566ef8b9871183
Tyler Laskey <tlaskey3@gmail.com>
2020-03-11 23:25:09 -0700

    add sending emails and cronjob implementation

--- node-webscraper/webscraper.js
+++ node-webscraper/webscraper.js
@@ -3,8 +3,10 @@
 const puppeteer = require('puppeteer')
 const CronJob = require('cron').CronJob
 const $ = require('cheerio')
+const nodemailer = require('nodemailer')
 
 const gtx1080ti = 'https://www.amazon.com/EVGA-Optimized-Interlaced-Graphics-11G-P4-6393-KR/dp/B07MFN49TK/'
+const gtx1060 = 'https://www.amazon.com/EVGA-GeForce-GAMING-Support-03G-P4-6162-KR/dp/B01KU2CIIY/'
 
 async function loadURLPage(url) {
     const browser = await puppeteer.launch()
@@ -13,7 +15,7 @@ async function loadURLPage(url) {
     return page
 }
 
-async function checkPrice(page) {
+async function checkPrice(page, url) {
     page.reload()
     let html = await page.evaluate(() => document.body.innerHTML)
     // console.log(html)
@@ -25,9 +27,37 @@ async function checkPrice(page) {
 
         if (currentPrice < 300) {
             console.log(dollarPrice + " Purchase that shit!")
+            sendNotification(currentPrice, url)
         }
         else console.log(dollarPrice + " Nah dawg..")
     })
+}
+
+async function sendNotification(price, url) {
+    let testAccount = await nodemailer.createTestAccount()
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass 
+        }
+    });
+
+    let textToSend = 'Price dropped to ' + price
+    let htmlText = '<a href=\"${url}\">Link</a>'
+
+    let info = await transporter.sendMail({
+        from: '"Price Tracker" <test@gmail.com>', 
+        to: "tlaskey3@gmail.com", 
+        subject: "Price update on GTX 1080ti: " + price, 
+        text: textToSend, 
+        html: htmlText
+    });
+
+    console.log("Message sent: $s", info.messageId)
 }
 
 async function startTracking() {
@@ -40,4 +70,14 @@ async function startTracking() {
     job.start()
 }
 
-startTracking()
+async function monitor(urlList) {
+    urlList.forEach(async url => {
+        const page = await loadURLPage(url)
+        await checkPrice(page, url)
+    })
+}
+
+const urlList = [gtx1080ti, gtx1060]
+monitor(urlList)
+
+// startTracking()
