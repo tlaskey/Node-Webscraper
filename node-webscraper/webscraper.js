b4b67c2c2e441f211d2b027a1fa97f35da75a179
Tyler Laskey <tlaskey3@gmail.com>
2020-03-11 22:19:01 -0700

    add standard linter and grab html body of webpage

--- node-webscraper/webscraper.js
+++ node-webscraper/webscraper.js
@@ -4,10 +4,17 @@ const puppeteer = require('puppeteer')
 const cron = require('cron').CronJob
 const cheerio = require('cheerio')
 
-const url = 'https://www.amazon.com/EVGA-Optimized-Interlaced-Graphics-11G-P4-6393-KR/dp/B07MFN49TK/'
+const gtx1080ti = 'https://www.amazon.com/EVGA-Optimized-Interlaced-Graphics-11G-P4-6393-KR/dp/B07MFN49TK/'
 
-(async () => {
+async function loadURLPage(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(url)
-})
+}
+
+async function checkPrice() {
+    page.reload()
+    let html = await page.evaluate(() => document.body.innerHTML)
+    console.log(html)
+    //priceblock_ourprice
+}
